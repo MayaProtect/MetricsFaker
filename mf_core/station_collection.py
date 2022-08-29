9aba81180a325989d46ab5a3c418a526e663bfaa
Peter Fontaine <peter.fontaine@reactiva.fr>
2022-08-29 21:08:00 +0200

    feat: Add capabilities to station collection to create station

--- mf_core/station_collection.py
+++ mf_core/station_collection.py
@@ -1,5 +1,5 @@
 from uuid import UUID
-
+from mf_core.station import Station
 
 class StationCollection(list):
     def __init__(self):
@@ -7,7 +7,10 @@ class StationCollection(list):
         self.__last_collect_timestamp = 0
 
     def create_station(self) -> UUID:
-        pass
+        station = Station()
+        uuid = station.uuid
+        self.append(station)
+        return uuid
 
     def collect_data(self) -> list:
         pass
