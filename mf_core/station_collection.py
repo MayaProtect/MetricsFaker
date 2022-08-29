fd564ecb104fd4c2cb62199f0d202bb87115d183
Peter Fontaine <peter.fontaine@reactiva.fr>
2022-08-29 21:15:45 +0200

    feat: add capabilitie for station collection to collect data from station

--- mf_core/station_collection.py
+++ mf_core/station_collection.py
@@ -1,5 +1,7 @@
 from uuid import UUID
+from time import time
 from mf_core.station import Station
+
 
 class StationCollection(list):
     def __init__(self):
@@ -13,7 +15,18 @@ class StationCollection(list):
         return uuid
 
     def collect_data(self) -> list:
-        pass
+        data = []
+        timestamp = int(time())
+        for station in self:
+            uuid = str(station.uuid)
+            station_data = station.metrics_history.get_lines_between(self.__last_collect_timestamp, timestamp)
+            for sd in station_data:
+                line = {"station_id": uuid, "timestamp": sd.timestamp, "temperature": sd.temperature, "sun": sd.sun,
+                        "rain": sd.rain, "battery_state": sd.battery_state, "wind": sd.wind}
+                data.append(line)
+
+        self.__last_collect_timestamp = timestamp if timestamp > self.__last_collect_timestamp else (timestamp + 1)
+        return data
 
     def collect_hives_data(self) -> list:
         pass
